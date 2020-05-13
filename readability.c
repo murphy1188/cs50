79afc3d48303474d5d178e3e3aa615fb64d02f30
murphy1188 <murphy1188@users.noreply.github.com>
2020-05-13 02:35:22 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -8,7 +8,11 @@ int main(void)
     int letters = 0;
     int words = 1;
     int sentences = 0;
+    // Prompt user to input reading sample to test
+    
     string s = get_string("Text: ");
+    
+    // Get counts of letters, words, and sentences from user input
     for (int i = 0, n = strlen(s); i < n; i++)
     {
         if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
@@ -24,10 +28,17 @@ int main(void)
             sentences++;
         }
     }
+    
+    // Calculate avg number of letters per 100 words
     float l = 100 / (float) words * letters;
+    
+    // Calculate avg number of sentences per 100 words
     float x = 100 / (float) words * sentences;
+    
+    // Calculate Coleman-Liau index
     float index = round((0.0588 * l) - (0.296 * x) - 15.8);
     
+    // Output reading level 
     if (index > 0 && index < 17)
     {
         printf("Grade %.0f\n", index);
