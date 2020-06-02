8666ba30dcc4f0e0123a6630769942de5506bb45
murphy1188 <murphy1188@users.noreply.github.com>
2020-06-02 03:26:42 +0000

    automated commit by submit50

---
+++ readability.py
@@ -0,0 +1,38 @@
+from cs50 import get_string
+
+# Initialize variables for letters, words, sentences
+letters = 0
+words = 1
+sentences = 0
+
+# Prompt user for text input
+s = get_string("Text: ")
+
+for c in s:
+    if (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z'):
+        letters += 1
+    elif " " in c:
+        words += 1
+    elif "." in c:
+        sentences += 1
+    elif "?" in c:
+        sentences += 1
+    elif "!" in c:
+        sentences += 1
+
+# Calculate avg number of letters per 100 words
+l = 100 / float(words) * letters
+
+#Calculate avg number of sentences per 100 words
+x = 100 / float(words) * sentences
+
+#Calculate Coleman-Liau index
+index = round((0.0588 * l) - (0.296 * x) - 15.8)
+
+#Output reading level
+if index > 0 and index < 17:
+    print(f"Grade {index}")
+elif index > 16:
+    print("Grade 16+")
+else:
+    print("Before Grade 1")
